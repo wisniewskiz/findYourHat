67bc533a41ad634f354b2f000663747a9f67f8bc
Zachary Wisniewski <wisniewskiz@live.com>
2021-03-30 13:13:50 -0600

    add correct game logic and fix prompt

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ let stillPlaying = true;
 let playerPosX = 0;
 let playerPosY = 0;
 
-
+///////////////////////////////////////
+//CREATING FIELD CLASS////////////////
+/////////////////////////////////////
 class Field {
     constructor(field) {
         this._field = field;
     }
-
     print() {
         for (let line of this._field) {
             console.log(line.join(' '));
@@ -29,51 +30,107 @@ class Field {
                 newField[h].push(fieldCharacter);
             }
         }
-        for (let n = 0; n < numHoles; n++) {
-            newField[Math.floor(Math.random() * height)][Math.floor(Math.random() * width)] = hole;
+        for (let n = 0; n < numHoles;) {
+            let coordX = Math.floor(Math.random() * height);
+            let coordY = Math.floor(Math.random() * width);
+            if (coordX != 0 && coordY != 0 && newField[coordX][coordY] != hole) {
+                newField[coordX][coordY] = hole;
+                n++;
+            }
         }
-        if (newField[Math.floor(Math.random() * height)][Math.floor(Math.random() * width)] = hat) { };
-        if (newField[0][0] = hole) {
-            newField[Math.floor(Math.random() * height)][Math.floor(Math.random() * width)] = hole;
-            newField[0][0] = pathCharacter;
-        } else {
-            newField[0][0] = pathCharacter;
-        }
+        for (let h = 0; h < 1;) {
+            let coordX = Math.floor(Math.random() * height);
+            let coordY = Math.floor(Math.random() * width);
+            if (newField[coordX][coordY] != hole && coordX != 0 && coordY != 0) {
+                newField[coordX][coordY] = hat;
+                h++;
+            }
+        };
+        newField[0][0] = pathCharacter;
+
         return newField;
     };
 };
 
+///////////////////////////////////////
+//GENERATING GAME FIELD///////////////
+/////////////////////////////////////
 let gameField = new Field(Field.generateField(5, 5, 3));
-console.log(gameField);
 gameField.print();
 
-//Starting game logic
-do {
-    let playerCommand = prompt('Which direction would you like to move? (u)p, (l)eft, (r)ight, or (d)own?');
-    switch (playerCommand) {
-        case 'u':
-            playerPosY -= 1;
-            break;
-        case 'l':
-            playerPosX -= 1;
-            break;
-        case 'r':
-            playerPosX += 1;
-            break;
-        case 'd':
-            playerPosY += 1;
-            break;
-        default:
-            console.log(`${playerCommand} is not a valid move. Please try again.`)
+///////////////////////////////////////
+//CREATING GAME LOGIC/////////////////
+/////////////////////////////////////
+console.log('Directions to move are: (u)p, (l)eft, (d)own, or (r)ight? \n');
+while (stillPlaying) {
+    let userDirection = prompt('Which Direction would you like to move?  : ');
+    
+    if(userDirection === 'u') {
+        playerPosY = playerPosY - 1;
+        if(playerPosY < 0) {
+            console.log('You fell of the game field. Game over');
+            stillPlaying = false;
+        } else if(gameField._field[playerPosY][playerPosX] === hole) {
+            console.log('You fell into a hole. Game over');
+            stillPlaying = false;
+        }else if (gameField._field[playerPosY][playerPosX] === hat) {
+            console.log('YOU WON! CONGRATULATIONS');
+            stillPlaying = false;
+        }  else{
+            gameField._field[playerPosY][playerPosX] = pathCharacter;
+            gameField.print();
+        };
     };
-    if(playerPosY >= 0 && playerPosX >=0) {
-        console.log(`New position is ${playerPosX}, ${playerPosY} with the space being occupied by ${gameField[playerPosY][playerPosX]}`);
-    } else {
-        console.log('You have fallen off the game field, game over');
-        stillPlaying = false;
-    }
+    if(userDirection === 'd') {
+        playerPosY = playerPosY + 1;
+        if(playerPosY > (gameField._field.length - 1)) {
+            console.log('You fell of the game field. Game over');
+            stillPlaying = false;
+        } else if(gameField._field[playerPosY][playerPosX] === hole) {
+            console.log('You fell into a hole. Game over');
+            stillPlaying = false;
+        } else if (gameField._field[playerPosY][playerPosX] === hat) {
+            console.log('YOU WON! CONGRATULATIONS');
+            stillPlaying = false;
+        } else{
+            gameField._field[playerPosY][playerPosX] = pathCharacter;
+            gameField.print();
+        };
+    };
+    if(userDirection === 'l') {
+        playerPosX = playerPosX - 1;
+        if(playerPosX < 0) {
+            console.log('You fell of the game field. Game over');
+            stillPlaying = false;
+        } else if(gameField._field[playerPosY][playerPosX] === hole) {
+            console.log('You fell into a hole. Game over');
+            stillPlaying = false;
+        }else if (gameField._field[playerPosY][playerPosX] === hat) {
+            console.log('YOU WON! CONGRATULATIONS');
+            stillPlaying = false;
+        }  else{
+            gameField._field[playerPosY][playerPosX] = pathCharacter;
+            gameField.print();
+        };
+    };
+    if(userDirection === 'r') {
+        playerPosX = playerPosX + 1;
+        if(playerPosX > (gameField._field[playerPosX].length - 1)) {
+            console.log('You fell of the game field. Game over');
+            stillPlaying = false;
+        } else if(gameField._field[playerPosY][playerPosX] === hole) {
+            console.log('You fell into a hole. Game over');
+            stillPlaying = false;
+        }else if (gameField._field[playerPosY][playerPosX] === hat) {
+            console.log('YOU WON! CONGRATULATIONS');
+            stillPlaying = false;
+        }  else{
+            gameField._field[playerPosY][playerPosX] = pathCharacter;
+            gameField.print();
+        };
+    };
+};
 
-} while(stillPlaying);
 
 
 
